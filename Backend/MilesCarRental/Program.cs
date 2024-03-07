705ac65b20b7e93a19ccadfa391e7ddcf91e53d6
UnTalYei <ylondonotz@gmail.com>
2024-03-07 13:30:54 -0500

    Actualización fuentes

--- Backend/MilesCarRental/Program.cs
+++ Backend/MilesCarRental/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<MilesCarRental>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MilesCarRental")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
