206115ea4986ac9f1c028118ea57fbfe45f9ac8e
UnTalYei <ylondonotz@gmail.com>
2024-03-07 21:12:35 -0500

    Update ILocationRepository.cs

--- Infrastructure/DataAccess/ILocationRepository.cs
+++ Infrastructure/DataAccess/ILocationRepository.cs
@@ -12,13 +12,6 @@ namespace Infrastructure.DataAccess;
 //}
 public interface ILocationRepository
 {
-    Task<IEnumerable<Location>> GetAllAsync();
-
-    Task<Location> GetByIdAsync(int id);
-
-    Task AddAsync(Location location);
-
-    Task UpdateAsync(Location location);
-
-    Task DeleteAsync(int id);
+    IEnumerable<Location> GetLocations();
+    Location GetLocationById(int id);
 }
